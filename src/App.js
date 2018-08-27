fc57e0493b7f51babbafcd35f8b5df42311175cb
Kenny Le <kennyle37@gmail.com>
2018-08-26 17:18:54 -0700

    Implement header nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Banner from './Banner/Banner'
+import HeaderBar from './Banner/Banner'
+
 
 class App extends React.Component {
   render() {
     return (
-      <Banner>
-        
-      </Banner>
+      <HeaderBar></HeaderBar>
     );
   }
 }
